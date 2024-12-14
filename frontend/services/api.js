b010f3ffcfbd566bd5942c4a6f1aae3dedd45cb9
Ådne Longva <adnelongva@gmail.com>
2024-12-14 15:35:39 +0100

    finished youtube tutorial

---
+++ frontend/services/api.js
@@ -0,0 +1,19 @@
+
+const API_KEY = "3875de2bc419f584f01f0cc17ed04a5b";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json();
+    return data.results;
+};
+
+export const searchMovies = async (query) => {
+    const response = await fetch(
+        `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+        query
+        )}`
+    );
+    const data = await response.json();
+    return data.results;
+};
